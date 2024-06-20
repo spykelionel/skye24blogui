528fdc79047a4eeceee3d15d3800e2d1429b66c5
N-TITIAN <ndifon.titian@gmail.com>
2024-06-20 13:58:19 +0100

    changed to tailwind css

---
+++ script.js
@@ -0,0 +1,59 @@
+const blogTopics = document.getElementById("blog-topics");
+const guideAndTools = document.getElementById("guide-and-tools");
+const trendingTopics = document.getElementById("trending-topics");
+
+const topics = [
+  "Company",
+  "Design",
+  "Technology",
+  "Crypto",
+  "Artificial Intelligence",
+  "Work",
+];
+
+const _topics = [
+  "Design Thinking",
+  "Technology",
+  "Web3",
+  "Programming",
+  "Ai",
+  "IOT",
+  "Robotics",
+];
+
+const tools = [
+  "Guidelines",
+  "Mentorship",
+  "Tutorials",
+  "Training",
+  "Career",
+  "Self care",
+];
+
+function appendChildELements(
+  parent,
+  listItems,
+  child = "li",
+  attributes = [{ name: "class", value: "" }]
+) {
+  listItems.map((item) => {
+    let element = document.createElement(child);
+    element.textContent = item;
+    element.classList = attributes.map((att) => att.value);
+    attributes.map((attr) => {
+      element.setAttribute(attr.name, attr.value);
+    });
+    parent.appendChild(element);
+  });
+}
+
+appendChildELements(blogTopics, topics);
+appendChildELements(guideAndTools, tools);
+appendChildELements(
+  trendingTopics,
+  _topics,
+  (child = "span"),
+  (attributes = [
+    { attribute: "class", value: "br-5 p5 b-1 hover-grey text-tertiary" },
+  ])
+);
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["**/*.html"],
+  theme: {
+    colors: {
+      "primary-color": "#1c5cff",
+      "secondary-color": "#969696",
+      "tertiary-color": "#6b6a6a",
+      "white": "#fff",
+      "grey": "#dadada"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
